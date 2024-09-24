ce399f363fc4244e8004faa8d49b6088802cf733
hevin08 <hevinmatariya@gmail.com>
2024-09-24 18:30:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const localModule = require('./module');
+
+let ans = localModule.sum(10,2);
+
+let min = localModule.sub(10,2);
+
+let data = localModule.todoList;
+
+console.log(`Sum = ${ans}`);
+console.log(`Min = ${min}`);
+console.log(data);
---
+++ module.js
@@ -0,0 +1,27 @@
+const sum = (a,b) => {
+    return a + b;
+}
+
+const sub = (a,b) => {
+    return a - b;
+}
+
+const todoList = [
+    {
+        id: 1,
+        task: "reading",
+    },
+    
+    {
+        id: 2,
+        task: "playing",
+    },
+
+    {
+        id: 3,
+        task: "craying",
+    },
+]
+module.exports={
+    sum,sub,todoList
+}
